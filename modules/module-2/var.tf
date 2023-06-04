872dcb7c5ec564f5f94105b816727a6088714442
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-04 20:55:41 +0530

    Update var.tf

--- modules/module-2/var.tf
+++ modules/module-2/var.tf
@@ -1,32 +1,49 @@
 variable "ami_instance" {
     type = string
-    default = "ami-08e5424edfe926b43"
+    value = "ami-0ed99df77a82560e6"
+} 
+
+variable "instance_name"{
+    type = string
+   value = "my_default_instance"
 }
 
 variable "instance_type" {
     type = string
-    default = "t2.micro"
-}
-variable "key_pair" {
-    type = string
-    default = "mykeypair"
+    value = "t2.medium"
 }
 
-variable "instance_name"{
-    type = string
-   default = "my_default_instance"
-}
-variable "instance_type2" {
-    type = string
-    default = "t2.micro"
-}
 variable "instance_name2"{
     type = string
-   default = "my_default_instance"
+   value = "worker-1"
 }
+
+variable "instance_type2" {
+    type = string
+    value = "t2.micro"
+}
+
+variable "instance_name3"{
+    type = string
+   value = "worker-2"
+}
+
+variable "instance_type3" {
+    type = string
+    value = "t2.micro"
+}
+
+variable "key_pair" {
+    type = string
+    default = "tokyo"
+}
+
 variable "subnet_id" {
- type = string 
+ type = string
+ value =
 }
+
 variable "vpc_id" {
     type = string
+    value =
 }
